292c78e4a8eec5d3345de806ce03fea0c64559b6
Gareth <me@gareth.codes>
2016-11-20 11:51:04 -0800

    starting to add checksums and FEC

--- yardstick.py
+++ yardstick.py
@@ -1,19 +1,23 @@
 from rflib import *
 import sys
 
+# Accessing RF registers
+#hex(ord(d.peek(PKTCTRL1)))
+
 def init(d):
     d.setMdmModulation(MOD_ASK_OOK)
     d.setFreq(433000000)
     d.setMdmSyncMode(2)
     d.setMdmSyncWord(0xEEEE)
-    d.setMdmDRate(1394)
+    d.setMdmDRate(1500)
+    #d.makePktFLEN(10)
     d.makePktVLEN(61)
-    d.setRFRegister(PKTCTRL1, 0x06) # set packet filtering
-    # Packet length from TX is payload minus length byte
+    d.setRFRegister(PKTCTRL1, 0x01) # set packet filtering but no status
+    #d.setRFRegister(PKTCTRL1, 0x06) # set packet filtering
     d.setPktAddr(0xdb)
-    d.setMdmNumPreamble(0)
+    d.setMdmNumPreamble(2)
     #d.setPktPQT(3)
-    d.setMaxPower()
+    #d.setMaxPower()
 
 
     #d.printRadioConfig()
